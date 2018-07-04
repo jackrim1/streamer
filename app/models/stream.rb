aee8def14ea5ac0eb05887e4611f0feff7b87746
jackrim1 <jriminton@gmail.com>
2018-07-04 22:41:01 +0100

    pic max size

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -3,4 +3,13 @@ class Stream < ApplicationRecord
 	default_scope -> { order(created_at: :desc) }
 	mount_uploader :picture, PictureUploader
 	validates :user_id, presence: true
+	#validate :picture_size
+
+	private
+
+	def picture_size
+		if picture_size > 5.megabytes
+			errors.add(:picture, "Should be less than 5MB")
+		end
+	end
 end
